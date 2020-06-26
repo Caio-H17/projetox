df84e088680da11ddc996bbf1829fec88d807f38
Caio-H17 <henrique_dias17@outlook.com>
2020-06-25 22:31:20 -0300

    Agora sim, tudo finalizad u.u

--- _include/footer.php
+++ _include/footer.php
@@ -1,10 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://github.com/Caio-H17/projetox"><i class="fab fa-github-alt"></i> Git - Caio_Henrique</a>.</strong>
+    <strong>Copyright &copy; 2020 <a target="_blank" href="https://github.com/Caio-H17/projetox"><i class="fab fa-github-alt"></i> Git - Caio_Henrique</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.5
     </div>
-    <a class="scroll-to-top rounded" href="#top">
-    <i class="fas fa-angle-up"></i>
-  </a>
   </footer>
